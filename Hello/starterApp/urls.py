fda7fb51e4a93913ef2f0312fce0e6eab6fe090b
denis-spe <denisbrian07@gmail.com>
2022-06-11 00:51:41 +0200

    Add register and login html file

--- Hello/starterApp/urls.py
+++ Hello/starterApp/urls.py
@@ -2,8 +2,14 @@
 from . import views                         
 from django.urls import path
 
+# Instantiate the App name
+app_name = "starterApp"
+
 # Construct a urlpatterns
 urlpatterns = [
-    path(route="", view=views.index, name="index")
+    path(route="", view=views.index, name="index"),
+    path(route="register", view=views.register,
+    name="register"),
+    path(route='login', view=views.login, name='login')
 ]
 
--- Hello/starterApp/views.py
+++ Hello/starterApp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django import forms
 
 # Create your views here.
 def index(request):
     return render(request, 'starterApp/index.html')
+    
+    
+def register(request):
+    return render(request, 'starterApp/register.html', {
+    'title': 'Register'
+    })
+    
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+  
+def login(request):
+    return render(request, 'starterApp/login.html', {
+    'forms': LoginForm(request.post)
+    })
