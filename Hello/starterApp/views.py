e21a69dccf9ed04e6230fadac09f99917b1a44a0
denis-spe <denisbrian07@gmail.com>
2022-06-18 21:24:28 +0200

    fixed the error in login page

--- Hello/starterApp/views.py
+++ Hello/starterApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django import forms
+from django.core import validators
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 # Create your views here.
 def index(request):
@@ -13,9 +17,23 @@ def register(request):
     
 
 class LoginForm(forms.Form):
-    email = forms.EmailField()
-  
+   username = forms.CharField(label='Username', max_length=100)
+   password = forms.CharField(
+   widget=forms.PasswordInput(),
+   label="Password",
+   validators=[
+   validators.MaxLengthValidator(limit_value=8, message="Password Must Have A Length Of eight(8) character")
+   ]
+   )
 def login(request):
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        
+        if login_form.is_valid():
+            return HttpResponseRedirect(reverse("index"))
+    else:
+        login_form = LoginForm()
+    
     return render(request, 'starterApp/login.html', {
-    'forms': LoginForm(request.post)
+    'forms':  login_form
     })
